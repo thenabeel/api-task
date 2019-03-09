56fe0667d90d8a206fe012cbe5db86dba4dcb973
Nabeel Ahmed <goodboynabeel@gmail.com>
2019-03-09 20:18:03 +0500

    Implemented External Books API

---
+++ app/Providers/IceAndFireServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Providers;
+
+use GuzzleHttp\Client;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\Support\DeferrableProvider;
+use App\Services\IceAndFire\IceAndFire;
+
+class IceAndFireServiceProvider extends ServiceProvider implements DeferrableProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton(IceAndFire::class, function () {
+            $client = new Client([
+                'base_uri' => 'https://www.anapioficeandfire.com/api/',
+            ]);
+
+            return new IceAndFire($client);
+        });
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    public function provides()
+    {
+        return [IceAndFire::class];
+    }
+}
